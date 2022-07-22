c8a6eef7654f526fa0992674204f82b720a518e4
Lucila7 <109292195+Lucila7@users.noreply.github.com>
2022-07-22 14:26:52 -0300

    first commit

---
+++ src/atalhos/Atal.java
@@ -0,0 +1,24 @@
+package atalhos;
+
+import atalhos.model.Gato;
+
+public class Atal {
+
+	public static  void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+		Gato gato = new Gato();
+		Livros livros = new Livros();
+		
+		System.out.println(gato);
+		System.out.println(livros);
+	}
+
+}
+
+class Livros {
+	private String nome;
+	private String npag;
+	
+}
